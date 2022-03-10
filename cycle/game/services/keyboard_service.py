da9bbef95c5e6ed91217b96f07161fc4b5e2ca36
Samuel Jessop Kaleopa <97277648+Bambyboi@users.noreply.github.com>
2022-03-11 06:16:43 +1300

    Update keyboard_service.py

--- cycle/game/services/keyboard_service.py
+++ cycle/game/services/keyboard_service.py
@@ -14,12 +14,12 @@ class KeyboardService:
     def __init__(self):
         """Constructs a new KeyboardService."""
         self._keys = {}
-        # if snake:
+        
         self._keys['w'] = pyray.KEY_W
         self._keys['a'] = pyray.KEY_A
         self._keys['s'] = pyray.KEY_S
         self._keys['d'] = pyray.KEY_D
-        # if snake2:
+
         self._keys['i'] = pyray.KEY_I
         self._keys['j'] = pyray.KEY_J
         self._keys['k'] = pyray.KEY_K
