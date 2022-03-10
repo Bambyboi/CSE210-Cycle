a15b8a07568be3a4851f3cfd1176530530916b29
Samuel Jessop Kaleopa <97277648+Bambyboi@users.noreply.github.com>
2022-03-11 06:11:40 +1300

    Update control_actors_action.py

--- cycle/game/scripting/control_actors_action.py
+++ cycle/game/scripting/control_actors_action.py
@@ -6,7 +6,7 @@ from game.shared.point import Point
 class ControlActorsAction(Action):
     """
     An input action that controls the snake.
-    
+
     The responsibility of ControlActorsAction is to get the direction and move the snake's head.
 
     Attributes:
@@ -15,7 +15,7 @@ class ControlActorsAction(Action):
 
     def __init__(self, keyboard_service):
         """Constructs a new ControlActorsAction using the specified KeyboardService.
-        
+
         Args:
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
@@ -32,23 +32,34 @@ class ControlActorsAction(Action):
         # left
         if self._keyboard_service.is_key_down('a'):
             self._direction = Point(-constants.CELL_SIZE, 0)
-        
+
         # right
         if self._keyboard_service.is_key_down('d'):
             self._direction = Point(constants.CELL_SIZE, 0)
-        
+
         # up
         if self._keyboard_service.is_key_down('w'):
             self._direction = Point(0, -constants.CELL_SIZE)
-        
+
         # down
         if self._keyboard_service.is_key_down('s'):
             self._direction = Point(0, constants.CELL_SIZE)
-        
+
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._direction)
 
+
 class ControlActorsAction2(ControlActorsAction):
+    def __init__(self, keyboard_service):
+        """Constructs a second ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+        super().__init__(keyboard_service)
+        self._keyboard_service = keyboard_service
+        self._direction = Point(constants.CELL_SIZE, 0)
+
     def execute(self, cast, script):
         """Executes the control actors action.
 
@@ -59,18 +70,18 @@ class ControlActorsAction2(ControlActorsAction):
         # left
         if self._keyboard_service.is_key_down('j'):
             self._direction = Point(-constants.CELL_SIZE, 0)
-        
+
         # right
         if self._keyboard_service.is_key_down('l'):
             self._direction = Point(constants.CELL_SIZE, 0)
-        
+
         # up
         if self._keyboard_service.is_key_down('i'):
             self._direction = Point(0, -constants.CELL_SIZE)
-        
+
         # down
         if self._keyboard_service.is_key_down('k'):
             self._direction = Point(0, constants.CELL_SIZE)
-        
+
         snake2 = cast.get_second_actor("snakes")
         snake2.turn_head(self._direction)
