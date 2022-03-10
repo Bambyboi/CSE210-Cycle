d3f9d5a30cde706fdc955cccf45bade98a0e4c72
Samuel Jessop Kaleopa <97277648+Bambyboi@users.noreply.github.com>
2022-03-11 06:07:55 +1300

    Update snake.py

--- cycle/game/casting/snake.py
+++ cycle/game/casting/snake.py
@@ -6,12 +6,13 @@ from game.shared.point import Point
 class Snake(Actor):
     """
     A long limbless reptile.
-    
+
     The responsibility of Snake is to move itself.
 
     Attributes:
         _points (int): The number of points the food is worth.
     """
+
     def __init__(self):
         super().__init__()
         self._segments = []
@@ -40,27 +41,27 @@ class Snake(Actor):
             velocity = tail.get_velocity()
             offset = velocity.reverse()
             position = tail.get_position().add(offset)
-            
+
             segment = Actor()
             segment.set_position(position)
             segment.set_velocity(velocity)
             segment.set_text("#")
-            segment.set_color(constants.BLUE)
+            segment.set_color(constants.GREEN)
             self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
-    
+
     def _prepare_body(self):
         x = int(constants.MAX_X / 2)
         y = int(constants.MAX_Y / 2)
 
         for i in range(constants.SNAKE_LENGTH):
-            position = Point(x - i * constants.CELL_SIZE, y)
+            position = Point(x - i * constants.CELL_SIZE, 200)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.BLUE if i == 0 else constants.BLUE
-            
+            color = constants.YELLOW if i == 0 else constants.GREEN
+
             segment = Actor()
             segment.set_position(position)
             segment.set_velocity(velocity)
