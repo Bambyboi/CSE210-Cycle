8ebb83798ae5099482215641213809d7548042b4
Samuel Jessop Kaleopa <97277648+Bambyboi@users.noreply.github.com>
2022-03-11 06:14:44 +1300

    Update move_actors_action.py

--- cycle/game/scripting/move_actors_action.py
+++ cycle/game/scripting/move_actors_action.py
@@ -1,21 +1,22 @@
 from game.scripting.action import Action
 
 
+# TODO: Implement MoveActorsAction class here!
+# Action class will be overriden
 class MoveActorsAction(Action):
-    """
-    An update action that moves all the actors.
-    
-    The responsibility of MoveActorsAction is to move all the actors that have a velocity greater
-    than zero.
-    """
 
+    # Override the execute(cast, script) method as follows:
     def execute(self, cast, script):
-        """Executes the move actors action.
+        """Executes something that is important in the game. This method should be overriden by 
+        derived classes.
 
         Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
+        # 1) get all the actors method from the cast
         actors = cast.get_all_actors()
+        # 2) loop through the actors
         for actor in actors:
+            # 3) call the move_next() method on each actor
             actor.move_next()
