858e7a3958eb0e0ce8b593a27f57a5b9b31f162a
Samuel Jessop Kaleopa <97277648+Bambyboi@users.noreply.github.com>
2022-03-11 06:08:41 +1300

    Update snake2.py

--- cycle/game/casting/snake2.py
+++ cycle/game/casting/snake2.py
@@ -3,7 +3,6 @@ from game.casting.actor import Actor
 from game.shared.point import Point
 from game.casting.snake import Snake
 
-
 class Snake2(Snake):
     """
     Second long limbless reptile.
@@ -19,22 +18,22 @@ class Snake2(Snake):
         self._segments = []
         self._prepare_body()
 
-    # def get_segments(self):
-    #     return self._segments
+    def get_segments(self):
+        return self._segments
 
-    # def move_next(self):
-    #     # move all segments
-    #     for segment in self._segments:
-    #         segment.move_next()
-    #     # update velocities
-    #     for i in range(len(self._segments) - 1, 0, -1):
-    #         trailing = self._segments[i]
-    #         previous = self._segments[i - 1]
-    #         velocity = previous.get_velocity()
-    #         trailing.set_velocity(velocity)
+    def move_next(self):
+        # move all segments
+        for segment in self._segments:
+            segment.move_next()
+        # update velocities
+        for i in range(len(self._segments) - 1, 0, -1):
+            trailing = self._segments[i]
+            previous = self._segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
 
-    # def get_head(self):
-    #     return self._segments[0]
+    def get_head(self):
+        return self._segments[0]
 
     def grow_tail(self, number_of_segments):
         for i in range(number_of_segments):
@@ -47,11 +46,11 @@ class Snake2(Snake):
             segment.set_position(position)
             segment.set_velocity(velocity)
             segment.set_text("#")
-            segment.set_color(constants.GREEN)
+            segment.set_color(constants.BLUE)
             self._segments.append(segment)
 
-    # def turn_head(self, velocity):
-    #     self._segments[0].set_velocity(velocity)
+    def turn_head(self, velocity):
+        self._segments[0].set_velocity(velocity)
 
     def _prepare_body(self):
         x = int(constants.MAX_X / 2)
@@ -61,7 +60,7 @@ class Snake2(Snake):
             position = Point(x - i * constants.CELL_SIZE, 400)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.GREEN if i == 0 else constants.GREEN
+            color = constants.RED if i == 0 else constants.BLUE
 
             segment = Actor()
             segment.set_position(position)
